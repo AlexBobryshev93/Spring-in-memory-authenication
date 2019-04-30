dbea387aa3505d6ac47c7f8e1c35d4992f44c0e7
AlexBobryshev <darknessrider1@rambler.ru>
2019-04-30 23:46:28 +0300

    initial commit

---
+++ src/main/java/com/example/builtinusers/BuiltInUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.builtinusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiltInUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuiltInUsersApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/builtinusers/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.builtinusers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+
+        /*Prior to Spring Security 5.0 the default PasswordEncoder was
+NoOpPasswordEncoder which required plain text passwords.
+In Spring Security 5, the default is DelegatingPasswordEncoder, which required Password Storage Format.
+That's why {noop} is needed*/
+
+        auth
+                .inMemoryAuthentication()
+                    .withUser("user")
+                        .password("{noop}password")
+                        .authorities("ROLE_USER")
+                    .and()
+                    .withUser("user1")
+                        .password("{noop}password")
+                        .authorities("ROLE_USER");
+
+    }
+}
